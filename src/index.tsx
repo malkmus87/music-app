b091df8f23a6b7d587ba38ff867b8e71b17a4fbb
malkmus87 <sebjoh1987@gmail.com>
2021-02-20 16:57:06 +0100

    First commit

---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import * as ReactDOM from 'react-dom';
+import * as React from 'react';
+function Index(){
+    return(<div>TestAre</div>)
+}
+ReactDOM.render(<Index/>,document.getElementById('root'));
