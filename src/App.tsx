1aeb1eed51f8be2b93ef9f2cd84718910c1fc4d9
malkmus87 <sebjoh1987@gmail.com>
2021-03-13 11:29:18 +0100

    new commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import * as React from 'react';
 import {FunctionComponent} from 'react';
 import { BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom';
-
+import PageTop from './components/PageTop/index';
 const App:FunctionComponent<void> = () => {
     return(
-        <Router>
+        <div>
+            <PageTop
+                
+            />
+            <Router>
             
-        </Router>
+            </Router>
+        </div>
+        
     )
 }
 export default App;
